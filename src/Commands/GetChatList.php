35094924014b4a8b42bc042480a6f2f389216304
Andrey Budiharja <andreybudiharja4@gmail.com>
2021-02-11 16:51:53 +0700

    get chat list command

---
+++ src/Commands/GetChatList.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Ejabberd\Commands;
+
+
+use Ejabberd\Commands\Contracts\IEjabberdCommand;
+
+class GetChatList implements IEjabberdCommand
+{
+    private $user;
+    private $host;
+    private $resource;
+
+    public function __construct($user, $host, $resource)
+    {
+        $this->user = $user;
+        $this->host = $host;
+        $this->resource = $resource;
+    }
+
+    public function getCommandName()
+    {
+        return 'send_stanza_c2s';
+    }
+
+    public function getCommandData()
+    {
+        return [
+            'user' => $this->user,
+            'host' => $this->host,
+            'resource' => $this->resource,
+            'stanza' => "<iq type='get' id='iq_stanza_id'><inbox xmlns='urn:xmpp:inbox:1'/></iq>"
+        ];
+    }
+}
